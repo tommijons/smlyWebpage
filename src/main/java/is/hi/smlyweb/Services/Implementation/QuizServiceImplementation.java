dcc6f8a6ba8ce0cb6e3b461237bd00b146b52456
katja8798 <katja8798@gmail.com>
2021-12-15 09:45:01 +0000

    Spurningar bætt við

--- src/main/java/is/hi/smlyweb/Services/Implementation/QuizServiceImplementation.java
+++ src/main/java/is/hi/smlyweb/Services/Implementation/QuizServiceImplementation.java
@@ -48,6 +48,34 @@ public class QuizServiceImplementation implements QuizService {
         quizRepository.save(new Question(0, "What is scriptPubKey ?","A script which controls how bitcoin can be spent","A script which unlocks a UTXO for spending","A script which contains all UTXO","A script which controls how bitcoin can be spent","A script which verifies a private key"));
         quizRepository.save(new Question(0, "What kind of string is used to write data on the blockchain ?","A hex string","A binary string","An octal string","A hex string","A regular string"));
        */
+
+        /*Questions about the commands
+        quizRepository.save(new Question(0, "What statement about the command addchapter is true ?","It adds a new chapter to book chapter service","It adds a new chapter to book chapter service","It adds a new book chapter to library service","It's a trick question, the command doesn't exist","It adds a new chapter from a specified book to your wallet by donating some SMLYs from your wallet, you can read it in the blockchain"));
+        quizRepository.save(new Question(0, "What statement about the command adddex is true ?","It adds a new DEX address to DEX service","It adds a new DEX service to DEX address","It adds a new DeX feature to the Samsung DeX feature","It adds a new DEX address to DEX service","It adds the old DEX service to DEX address"));
+        quizRepository.save(new Question(0, "What statement about the command addmultisigaddress is true ?","It adds a nrequired-to-sign multisignature address to the wallet","One key is generated for the Smileycoin address and each n person uses that one key","If 'account' is not specified, it generates an error","If 'nrequired' is not specified it defaults to n=1","It adds a nrequired-to-sign multisignature address to the wallet"));
+        quizRepository.save(new Question(0, "What statement about the command addnode is true ?","It attempts to add or remove a node from the addnode list","If the 'command' is subtraction then it subtracts a node from the list","If the 'command' is onechance then it has one chance to connect to the node","It attempts to add or remove a node from the addnode list","If the 'command' is addition then it adds a node to the list"));
+        quizRepository.save(new Question(0, "What statement about the command addubi is true ?","It adds a new UBI recipient to UBI service.","It has three required fields, servicename, ubiaddress and ubiwallet","It adds a new UBI recipient to UBI service.","",""));
+        quizRepository.save(new Question(0, "What statement about the command backupwallet is true ?","It safely copies wallet.dat to destination","It safely copies wallet.dat to destination","The destination field is only a path with filename",""The destination field is only a directory"","It rewrites wallet.dat"));
+        quizRepository.save(new Question(0, "What statement about the command buycoupon is true ?","It purchases a coupon on the blockchain","It has two required fields, couponaddress and couponamount","It has no required field/s","There is no such command","It purchases a coupon on the blockchain"));
+        */
+
+        /*Questions about SmileyCoin specifically
+        quizRepository.save(new Question(0, "How is the block reward split?","Miners fee, dividend and donation ?","It isn't split, it's used completely as the miners fee","Miners fee, dividend and donation","Miner's fee and donation","Miner's fee and dividend"));
+        quizRepository.save(new Question(0, "Why was Smileycoin initially developed ?","It was initially made as a way to test reward mechanisms in tutor-web and for fun","It was initially made as a way to test reward mechanisms in canvas","It was initially made as a gag","It was initially made as a way to test students who want to learn about cryptocurrencies","It was initially made as a way to test reward mechanisms in tutor-web and for fun"));
+        quizRepository.save(new Question(0, "What is a valid method to earn/get Smileycoins ?","Donating to Education in a Suitcase","Studying in canvas","Buy SMLY using trickery","Donating to Education in a Suitcase","By using a regular ATM"));
+        quizRepository.save(new Question(0, "What is Smileycoin designed for ?","It's designed for education and charity","It's designed for education and charity","It's designed for education","It's designed for charity","It's designed for profit"));
+        quizRepository.save(new Question(0, "When was Smileycoin launched ?","2014","2014","2012","2015","2013"));
+        quizRepository.save(new Question(0, "What is true about Education in a Suitcase ?","It's a charity program","It's a charity program","It's an education program","It's an internship program","It's a teachers elective program"));
+         */
+
+        /*Questions about cryptocurrency in general
+        quizRepository.save(new Question(0, "Who is the inventor of Bitcoin ?","The inventor is unknown","Steven Hawking","God","The inventor is unknown","John Moore"));
+        quizRepository.save(new Question(0, "What is true about cryptojacking ?","It's when scammers use someone else's machine’s resources to mine cryptocurrencies","It's also called corrupt cryptomining","It's a made up word","It's a valid and honest why to earn/get more crypto","It's when scammers use someone else's machine’s resources to mine cryptocurrencies"));
+        quizRepository.save(new Question(0, "Who is the largest crypto miner ?","China","China","America","Russia","England"));
+        quizRepository.save(new Question(0, "What country hasn't banned crypto ?","China","Bangladesh","Ecuador","China","Nepal"));
+        quizRepository.save(new Question(0, "What is the meaning of crypto ?","It is greek terminology for hidden or private","It is roman terminology for coins","It is egyptian terminology for mystery","It is latin terminology for money or payment","It is greek terminology for hidden or private"));
+        quizRepository.save(new Question(0, "Which of these are real cryptocurrencies ?","Ethereum","Smolcoin","Ethereum","Lighttap","Poppy"));
+         */
     }
 
 
